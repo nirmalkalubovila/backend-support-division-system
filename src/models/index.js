0939668e7caf2bdd8d2b6d77fbf59c410be9686b
thavishiminolka <155363547+thavishiminolka@users.noreply.github.com>
2026-06-12 10:07:27 +0530

    Removed duplicate project declarations

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,6 @@ const { Token, tokenTypes } = require('./auth/token.model');
 const ActivityLog = require('./system/activity-log.model');
 const Permission = require('./system/permission.model');
 const Role = require('./system/role.model');
-const Project = require('./projects/project.model');
 const Client = require('./project-management/client.model');
 const Project = require('./project-management/project.model');
 const Issue = require('./issue-management/issue.model');
@@ -17,6 +16,5 @@ module.exports = {
   Role,
   Project,
   Client,
-  Project,
   Issue,
 };
